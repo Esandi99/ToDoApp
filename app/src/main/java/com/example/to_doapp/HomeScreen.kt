cb58400ff332a99529b0c8d144736afd6c225aa4
Esandi99 <it22166692@my.sliit.lk>
2024-05-12 21:38:32 +0530

    splash Screen created

---
+++ app/src/main/java/com/example/to_doapp/HomeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.to_doapp
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_screen)
+        val delayMillis = 3000L // 3 seconds
+
+        Handler().postDelayed({
+            val intent = Intent(this@HomeScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMillis)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
